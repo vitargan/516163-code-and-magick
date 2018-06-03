dd749e8f1216049c1fc56803e473fbb4fe320f86
vitargan <36110701+vitargan@users.noreply.github.com>
2018-06-03 22:02:40 +0300

    домашка с координатами мага

--- js/code-and-magick.js
+++ js/code-and-magick.js
@@ -1,23 +1,23 @@
-var  fireballSize = 22;
+var fireballSize = 22;
 var wizardSpeed = 3;
-var wizardWidth =70;
-var getFireballSpeed = function(left) {
-if (true == left) {
-  return 5;
-}  else {
+var wizardWidth = 70;
+var getFireballSpeed = function (left) {
+  if (left === true) {
+    return 5;
+  } else {
     return 2;
   }
 };
-var getWizardHeight = function(WizardHeight) {
+var getWizardHeight = function (WizardHeight) {
   var calcWizardHeight = 1.337 * wizardWidth;
   return calcWizardHeight;
 };
 var wizardHeight = getWizardHeight(wizardWidth);
-var getWizardX = function(width) {
-  var positionWidth = (width*0.5)-wizardWidth/2
+var getWizardX = function (width) {
+  var positionWidth = (width * 0.5) - wizardWidth / 2;
   return positionWidth;
 };
-var getWizardY = function(height) {
-  var positionHeight = (height*1/3)
+var getWizardY = function (height) {
+  var positionHeight = (height * 1/3);
   return positionHeight;
-}
+};
